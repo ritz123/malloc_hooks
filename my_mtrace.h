6770d91bb8e149137c5673a46d7a27329c14c34b
Biplab Sarkar <sarkarbiplab@gmail.com>
2019-10-27 06:39:39 +0530

    c++

--- my_mtrace.h
+++ my_mtrace.h
@@ -5,5 +5,14 @@
 #ifndef MALLOC_HOOKS_MY_MTRACE_H
 #define MALLOC_HOOKS_MY_MTRACE_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void mtrace();
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif //MALLOC_HOOKS_MY_MTRACE_H
